7753be69c4d6e1b88dfb5537290ff9f0271d2ea8
Rasmilakc <139522774+Rasmilakc@users.noreply.github.com>
2023-10-01 22:02:46 -0500

    first commit

---
+++ test2.java
@@ -0,0 +1,23 @@
+package testng1;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+	@Test
+	public void testcase2() {
+		System.out.println("Test case 2");
+	}
+	@Test
+	public void testcase3() {
+		System.out.println("Test case 3");
+	}
+	@Test
+	public void testcase4() {
+		System.out.println("Test case 4");
+	}
+	@Test
+	public void testcase5() {
+		System.out.println("Test case 5");
+	}
+
+}
---
+++ testng.java
@@ -0,0 +1,12 @@
+package testng1;
+
+import org.testng.annotations.Test;
+
+public class testng {
+	@Test
+	public void Testcase1() {
+		System.out.println("Test case one");
+		
+	}
+
+}
