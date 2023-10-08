5378353a7aab10973bbdb19e9816270cb09c808c
Rasmilakc <139522774+Rasmilakc@users.noreply.github.com>
2023-10-07 21:59:54 -0500

    added

---
+++ test13a.java
@@ -0,0 +1,24 @@
+package testng1;
+
+import org.testng.annotations.Test;
+
+public class test13a {
+	public class test13 {
+		@Test(groups={"login"})
+		public void Testcase4() {
+			System.out.println("login  test1");
+		}
+		@Test(groups={"regression"})
+		public void Testcase5() {
+			System.out.println("regression test2");
+		}
+		@Test(groups={"login"})
+		public void Testcase6() {
+			System.out.println("login test3");
+		
+
+	}
+
+}
+
+}
