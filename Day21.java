c20a68c4b38dacb7dab711fc4e6720dd1114c46a
Rasmilakc <139522774+Rasmilakc@users.noreply.github.com>
2023-10-10 00:02:11 -0500

    Add files via upload

---
+++ Day21.java
@@ -0,0 +1,71 @@
+package seleniumtutorials;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String path="C:\\JAVA\\SeleniumTutorial\\src\\seleniumtutorials\\config.properties";
+	   
+	    Properties prop = new Properties();
+		
+		
+		try {
+
+			FileInputStream fileInput = new FileInputStream(path);
+			prop.load(fileInput);
+			String url = prop.getProperty("baseUrl");
+			String browser = prop.getProperty("browser");
+			String un = prop.getProperty("username");
+			String pd = prop.getProperty("password");
+
+			// writing to the file
+			prop.put("city", "pune");
+			FileOutputStream outputStrem = new FileOutputStream(path);
+			// Storing the properties file
+			prop.store(outputStrem, "This is a sample properties file");
+		
+
+
+			if (browser.equals("chrome")) {
+				// code to setup browser
+				System.setProperty("webdriver.chrome.driver", "C:\\Users\\ASUS\\OneDrive\\Desktop\\chromedriver\\chromedriver.exe");
+			    ChromeDriver driver=new ChromeDriver();
+				driver.get(url);
+				driver.findElement(By.cssSelector("#user-name")).sendKeys(un);
+				driver.findElement(By.cssSelector("#password")).sendKeys(pd);
+				driver.findElement(By.cssSelector("#login-button")).click();
+
+				if (driver.getCurrentUrl().contains("inventory")) {
+					System.out.println("Testcase pass");
+				} else {
+					System.out.println("Test case fail");
+				}
+
+			} else if (browser.equals("firefox")) {
+				// code to setup firefox
+
+			} else if (browser.equals("edge")) {
+				// code to edge
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+
+	}
+
+
